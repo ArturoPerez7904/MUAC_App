04ec6ed4965b75c78cdf4f451fc88dc41fec4eae
ArturoPerez7904 <arturoperez7904@gmail.com>
2023-06-21 10:00:50 -0500

    Added Files Model

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import Main from './components/Main';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import FileUploader from './components/FileUploader';
 
 function App() {
   const user = localStorage.getItem("token");
   return (
     <Routes>
       {user && <Route path="/" exact element={<Main/>}/>}
+      {user && <Route path="/files" exact element={<FileUploader/>}/>}
       <Route path = "/signup" exact element ={<Signup/>}/>
       <Route path = "/login" exact element ={<Login/>}/>
       <Route path = "/" exact element ={<Navigate replace to="/login" />}/>      
--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -16,6 +16,7 @@ const Main = () =>{
                 </button>
             </nav>
         </div>
+        
     )
 };
 
